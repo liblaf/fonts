8d1f3dcd1f543929c976aa1eb1123b47adf76702
liblaf <30631553+liblaf@users.noreply.github.com>
2025-10-05 13:33:52 +0800

    refactor(ci): Refine CI workflows and build script
    This commit introduces several improvements to the CI workflows and the `twemoji-flags` build script to enhance consistency, readability, and stability.
    
    Key changes include:
    - Pinning the `liblaf/actions/release` action to `@dist` instead of `@main`. This ensures that workflows use a stable, compiled version of the action, improving reliability.
    - Standardizing the use of single quotes (`'`) for strings and arguments in YAML and shell scripts for stylistic consistency.
    - Improving comments by adding an explanation for the cron schedule and standardizing the format for reference links to `ref: <url>`.

--- twemoji-flags/build.sh
+++ twemoji-flags/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-set -o errexit -o nounset -o pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
-gh release --repo "JoeBlakeB/ttf-twemoji-aur" download --clobber --output "Twemoji.ttf" --pattern "Twemoji-*.ttf"
-# https://github.com/talkjs/country-flag-emoji-polyfill/blob/527a3d121d2e198e733934a8b1007c819244e903/build/make-font.sh
-uvx --from fonttools pyftsubset "Twemoji.ttf" \
-  --no-subset-tables+="FFTM" \
-  --unicodes="U+1F1E6-1F1FF,U+1F3F4,U+E0061-E007A,U+E007F" \
-  --output-file="TwemojiSubset.ttf" \
+gh release --repo 'JoeBlakeB/ttf-twemoji-aur' download --clobber --output 'Twemoji.ttf' --pattern 'Twemoji-*.ttf'
+# ref: <https://github.com/talkjs/country-flag-emoji-polyfill/blob/master/build/make-font.sh>
+uvx --from fonttools pyftsubset 'Twemoji.ttf' \
+  --no-subset-tables+='FFTM' \
+  --unicodes='U+1F1E6-1F1FF,U+1F3F4,U+E0061-E007A,U+E007F' \
+  --output-file='TwemojiSubset.ttf' \
   --verbose \
   --timing
-uv run --with fonttools "https://github.com/fonttools/fonttools/raw/refs/heads/main/Snippets/rename-fonts.py" \
-  --suffix " Flags" \
-  --output-file "TwemojiFlags.ttf" \
-  "TwemojiSubset.ttf"
+uv run --with fonttools 'https://github.com/fonttools/fonttools/raw/refs/heads/main/Snippets/rename-fonts.py' \
+  --suffix ' Flags' \
+  --output-file 'TwemojiFlags.ttf' \
+  'TwemojiSubset.ttf'
